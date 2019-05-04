65cd16a237c25e564514626edfead44f073da160
Jess <iamseye@gmail.com>
2019-05-04 15:59:42 +0800

    inital

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: `Gatsby Default Starter`,
     description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    author: `Jessie Huang`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -25,6 +25,15 @@ module.exports = {
         theme_color: `#663399`,
         display: `minimal-ui`,
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+      },
+    },
+    `gatsby-plugin-sass`,
+    {
+      resolve: `gatsby-plugin-google-fonts`,
+      options: {
+        fonts: [
+          `Nunito Sans\:300,600,800`,
+        ]
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
