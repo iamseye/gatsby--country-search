125b30c886efc6e78026f3270fca3a9d516503a2
Jess <iamseye@gmail.com>
2019-05-18 15:07:41 +0800

    ADD: create page for detail page

--- gatsby-node.js
+++ gatsby-node.js
@@ -5,3 +5,55 @@
  */
 
 // You can delete this file if you're not using it
+const path = require('path');
+const slash = require('slash');
+
+exports.createPages = ({ actions, graphql }) => {
+  const { createPage } = actions;
+  const docTemplate = path.resolve('src/templates/detail.js');
+
+  // Individual doc pages
+  return graphql(`
+    {
+      allCountries {
+        edges {
+          node {
+            id
+            name
+            nativeName
+            alpha2Code
+            alpha3Code
+            region
+            subregion
+            borders
+            population
+            capital
+            borders
+            topLevelDomain
+            currencies {
+              name
+            }
+            languages{
+              name
+            }
+          }
+        }
+      }
+    }
+    `).then((result) => {
+    if (result.errors) {
+      Promise.reject(result.errors);
+    }
+
+    result.data.allCountries.edges.forEach(({ node }) => {
+      createPage({
+        path: `/country/${node.alpha2Code}/`,
+        component: slash(docTemplate),
+        context: {
+          id: node.id,
+          data: node,
+        },
+      });
+    });
+  });
+};
--- src/templates/detail.js
+++ src/templates/detail.js
@@ -2,13 +2,13 @@ import React from 'react';
 import { Link } from 'gatsby';
 import Layout from '../components/layout';
 import Image from '../components/image';
-import DetailContent from '../components/detailContent';
 import SEO from '../components/seo';
 
-const IndexPage = () => (
+const IndexPage = ({ pageContext }) => (
   <Layout>
     <SEO title="Detail" keywords={[`gatsby`, `application`, `react`]} />
-    <DetailContent />
+    {pageContext.id}
+    <div>{ pageContext.data.name }</div>
   </Layout>
 );
 
